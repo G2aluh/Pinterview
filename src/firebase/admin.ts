14673275936d774345a12a57953638f1f5b51f0d
galuhssaputra <galuhputrana576@gmail.com>
2025-03-30 20:36:25 +0700

    two commit

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,33 +1,23 @@
-import { cert, getApps, initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { getFirestore } from 'firebase-admin/firestore';
+import {cert, getApps, initializeApp} from 'firebase-admin/app';
+import {getAuth} from 'firebase-admin/auth';
+import {getFirestore} from 'firebase-admin/firestore';
+const initFirebaseAdmin = () => {
+    const apps = getApps();
 
-// Pastikan variabel lingkungan ada
-if (
-  !process.env.FIREBASE_PROJECT_ID ||
-  !process.env.FIREBASE_CLIENT_EMAIL ||
-  !process.env.FIREBASE_PRIVATE_KEY
-) {
-  throw new Error('Missing required Firebase Admin environment variables');
+    if(!apps.length){
+        initializeApp({
+            credential: cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            })
+        })
+    }
+
+    return {
+        auth: getAuth(),
+        db: getFirestore()
+    }
 }
 
-const initFirebaseAdmin = () => {
-  const apps = getApps();
-
-  if (!apps.length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
-  }
-
-  return {
-    auth: getAuth(),
-    db: getFirestore(),
-  };
-};
-
-export const { auth, db } = initFirebaseAdmin();
+export const {auth, db} = initFirebaseAdmin();
